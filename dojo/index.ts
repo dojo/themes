9af69893ceb3d92635c5ec81cb1a414cba5d3a06
Tom Dye <github@tomdye.co.uk>
2018-02-21 15:25:23 +0000

    rename icons to icon

--- dojo/index.ts
+++ dojo/index.ts
@@ -5,7 +5,7 @@ import * as checkbox from './checkbox.m.css';
 import * as comboBox from './comboBox.m.css';
 import * as enhancedtextinput from './enhancedtextinput.m.css';
 import * as dialog from './dialog.m.css';
-import * as icons from './icons.m.css';
+import * as icon from './icon.m.css';
 import * as label from './label.m.css';
 import * as listbox from './listbox.m.css';
 import * as progress from './progress.m.css';
@@ -30,7 +30,7 @@ export default {
 	'@dojo/widgets/comboBox': comboBox,
 	'@dojo/widgets/enhancedtextinput': enhancedtextinput,
 	'@dojo/widgets/dialog': dialog,
-	'@dojo/widgets/icons': icons,
+	'@dojo/widgets/icon': icon,
 	'@dojo/widgets/label': label,
 	'@dojo/widgets/listbox': listbox,
 	'@dojo/widgets/progress': progress,
