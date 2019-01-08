b179b730e7ee37f5f99befd4034a662af834f7e5
rorticus <rorticus@gmail.com>
2019-01-08 11:22:38 -0500

    Adding styles for range slider (#32)

--- src/dojo/index.ts
+++ src/dojo/index.ts
@@ -16,6 +16,7 @@ import * as label from './label.m.css';
 import * as listbox from './listbox.m.css';
 import * as progress from './progress.m.css';
 import * as radio from './radio.m.css';
+import * as rangeSlider from './range-slider.m.css';
 import * as select from './select.m.css';
 import * as slidePane from './slide-pane.m.css';
 import * as slider from './slider.m.css';
@@ -48,6 +49,7 @@ export default {
 	'@dojo/widgets/listbox': listbox,
 	'@dojo/widgets/progress': progress,
 	'@dojo/widgets/radio': radio,
+	'@dojo/widgets/range-slider': rangeSlider,
 	'@dojo/widgets/select': select,
 	'@dojo/widgets/slide-pane': slidePane,
 	'@dojo/widgets/slider': slider,
